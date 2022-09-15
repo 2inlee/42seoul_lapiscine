9c00a38ecd91335329ad2716b35c35a3be7e67a2
inho <95608811+AlphateamInho@users.noreply.github.com>
2022-09-15 20:51:26 +0900

    c02_85point

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inlee <inlee@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 20:48:41 by inlee             #+#    #+#             */
+/*   Updated: 2022/09/05 12:33:53 by inlee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	cnt;
+	unsigned int	i;
+
+	cnt = 0;
+	i = 0;
+	while (src[cnt] != '\0')
+		cnt++;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (cnt);
+}
---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inlee <inlee@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 21:12:52 by inlee             #+#    #+#             */
+/*   Updated: 2022/09/04 23:53:13 by inlee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int				i;
+	unsigned char	a;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		a = str[i];
+		if (str[i] >= 32 && str[i] != 127)
+		{
+			ft_putchar(a);
+		}
+		else
+		{
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[a / 16]);
+			ft_putchar("0123456789abcdef"[a % 16]);
+		}
+		i++;
+	}
+}
